393feb11c5cba4026990c7635578bfe1b977babf
Prathamesh Dukare <prathameshdukaremail@gmail.com>
2024-05-17 11:04:56 +0530

    add react flow

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,42 @@
-import "./App.css";
+import React, { useCallback } from "react";
 
-function App() {
-  return <main className="text-red-400">dssd</main>;
+import ReactFlow, {
+  MiniMap,
+  Controls,
+  useNodesState,
+  useEdgesState,
+  addEdge,
+} from "reactflow";
+
+import "reactflow/dist/style.css";
+
+const initialNodes = [
+  { id: "1", position: { x: 0, y: 0 }, data: { label: "1" } },
+  { id: "2", position: { x: 0, y: 100 }, data: { label: "2" } },
+];
+const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
+
+export default function App() {
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+  const onConnect = useCallback(
+    (params) => setEdges((eds) => addEdge(params, eds)),
+    [setEdges]
+  );
+
+  return (
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+      >
+        <Controls />
+        <MiniMap />
+      </ReactFlow>
+    </div>
+  );
 }
-
-export default App;
